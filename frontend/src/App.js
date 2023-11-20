458a461899d4b6d3a0544f2b0d8c780102c8845e
Mohammed Shakir <mohammedshakir010528@gmail.com>
2023-11-20 21:42:19 +0100

    fixed the db relationships and fixed some login bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,16 +11,20 @@ import './App.css';
 function App() {
   const isAuthenticated = localStorage.getItem('token');
 
+  const PrivateRoute = ({ children }) => {
+    return isAuthenticated ? children : <Navigate to="/login" />;
+  };
+
   return (
     <Router>
       <div>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/" element={isAuthenticated ? <Home /> : <Navigate to="/login" />} />
-          <Route path="/profile" element={isAuthenticated ? <UserProfile /> : <Navigate to="/login" />} />
-          <Route path="/events" element={isAuthenticated ? <Events /> : <Navigate to="/login" />} />
-          <Route path="/events/:eventId" element={isAuthenticated ? <EventDetails /> : <Navigate to="/login" />} />
+          <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
+          <Route path="/profile" element={<PrivateRoute><UserProfile /></PrivateRoute>} />
+          <Route path="/events" element={<PrivateRoute><Events /></PrivateRoute>} />
+          <Route path="/events/:eventId" element={<PrivateRoute><EventDetails /></PrivateRoute>} />
         </Routes>
       </div>
     </Router>
