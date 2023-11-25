9ab31bfe1eb7cd9cbe8a0e0b6ad449cb7e82876a
syko240 <andrepost@hotmail.se>
2023-11-25 21:48:27 +0100

    update and delete operations to events

--- backend/src/routes/events.js
+++ backend/src/routes/events.js
@@ -6,5 +6,7 @@ router.get('/', eventController.getEvents);
 router.post('/addEvent', eventController.addEvent);
 router.get('/:eventId', eventController.getEventById);
 router.post('/register/:eventId', eventController.registerForEvent);
+router.put('/:eventId', eventController.updateEvent);
+router.delete('/:eventId', eventController.deleteEvent);
 
 module.exports = router;
