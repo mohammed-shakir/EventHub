5da030d9070fbeb7f90e141afaf23cee00023dd1
andre <andrepost@hotmail.se>
2023-12-19 20:30:40 +0100

    User Controller testing

--- backend/tests/controllers/userController.test.js
+++ backend/tests/controllers/userController.test.js
@@ -1,21 +1,6 @@
 const request = require('supertest');
 const app = require('../../server');
 const pool = require('../../src/utils/database');
-const bcrypt = require('bcryptjs');
-
-jest.mock('../../src/utils/database', () => ({
-    query: jest.fn().mockImplementation((query, values) => {
-        if (query.includes('INSERT INTO Users')) {
-            return Promise.resolve({ rows: [{ user_id: 1 }], rowCount: 1 });
-        }
-        return Promise.resolve({ rows: [], rowCount: 0 });
-    }),
-}));
-
-jest.mock('bcryptjs', () => ({
-    ...jest.requireActual('bcryptjs'),
-    compare: jest.fn().mockResolvedValue(true),
-}));
 
 describe('UserController Tests', () => {
     describe('POST /api/users/register', () => {
@@ -35,25 +20,63 @@ describe('UserController Tests', () => {
             expect(response.statusCode).toBe(201);
         });
     });
-});
 
-describe('POST /api/users/login', () => {
-    it('should log in a user successfully', async () => {
-        pool.query.mockResolvedValueOnce({
-            rows: [{ user_id: 1, password: 'hashedPassword' }],
-            rowCount: 1
+    describe('POST /api/users/register', () => {
+        it('should register a new user unsuccessfully', async () => {
+            const newUser = {
+                email: 'test@example.com',
+                password: 'password123',
+                first_name: 'Test',
+                last_name: 'User',
+                user_role: 'Regular'
+            };
+
+            const response = await request(app)
+                .post('/api/users/register')
+                .send(newUser);
+
+            expect(response.statusCode).toBe(409);
         });
+    });
 
-        const userCredentials = {
-            email: 'test@example.com',
-            password: 'password123'
-        };
+    describe('POST /api/users/login', () => {
+        it('should log in the user successfully', async () => {
+            const loginUser = {
+                email: 'test@example.com',
+                password: 'password123',
+            };
 
-        const response = await request(app)
-            .post('/api/users/login')
-            .send(userCredentials);
+            const response = await request(app)
+                .post('/api/users/login')
+                .send(loginUser);
 
-        expect(response.statusCode).toBe(200);
-        expect(response.body).toHaveProperty('token');
+            expect(response.statusCode).toBe(200);
+            token = response.body.token;
+        });
+    });
+
+    describe('POST /api/users/login', () => {
+        it('should log in the user unsuccessfully', async () => {
+            const loginUser = {
+                email: 'wonrg_user@example.com',
+                password: 'password123',
+            };
+
+            const response = await request(app)
+                .post('/api/users/login')
+                .send(loginUser);
+
+            expect(response.statusCode).toBe(404);
+        });
+    });
+
+    describe('DELETE /api/users/profile', () => {
+        it('should delete the registered user', async () => {
+            const response = await request(app)
+                .delete('/api/users/profile')
+                .set('Authorization', `Bearer ${token}`);
+
+            expect(response.statusCode).toBe(200);
+        });
     });
 });
