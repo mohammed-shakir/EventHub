32b0161df555096050d33c9b5270b99134da5ba1
andre <andrepost@hotmail.se>
2023-12-19 19:40:05 +0100

    login test

--- backend/tests/controllers/userController.test.js
+++ backend/tests/controllers/userController.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const app = require('../../server');
 const pool = require('../../src/utils/database');
+const bcrypt = require('bcryptjs');
 
 jest.mock('../../src/utils/database', () => ({
     query: jest.fn().mockImplementation((query, values) => {
@@ -9,6 +10,11 @@ jest.mock('../../src/utils/database', () => ({
         }
         return Promise.resolve({ rows: [], rowCount: 0 });
     }),
+}));
+
+jest.mock('bcryptjs', () => ({
+    ...jest.requireActual('bcryptjs'),
+    compare: jest.fn().mockResolvedValue(true),
 }));
 
 describe('UserController Tests', () => {
@@ -28,5 +34,26 @@ describe('UserController Tests', () => {
 
             expect(response.statusCode).toBe(201);
         });
+    });
+});
+
+describe('POST /api/users/login', () => {
+    it('should log in a user successfully', async () => {
+        pool.query.mockResolvedValueOnce({
+            rows: [{ user_id: 1, password: 'hashedPassword' }],
+            rowCount: 1
+        });
+
+        const userCredentials = {
+            email: 'test@example.com',
+            password: 'password123'
+        };
+
+        const response = await request(app)
+            .post('/api/users/login')
+            .send(userCredentials);
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('token');
     });
 });
